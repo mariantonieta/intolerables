a237effa4910f7d9476b6034880e3832b9733c9e
Mariantonieta <Mariantonietachacondibacco@gmail.com>
2025-03-30 22:44:41 +0200

    Agregando modal-intolerancia

--- src/pages/intolerancias/index.tsx
+++ src/pages/intolerancias/index.tsx
@@ -1,10 +1,33 @@
-import IntoleranciaCard from "../../components/IntolerancaCard";
+import IntoleranciaCard from "../../components/cardintole/IntolerancaCard";
 import Navigation from "../../containers/navigation";
 import gluten from "../../../public/icons/gluten.svg";
 import "./index.css";
+import { useState } from "react";
+import Modal from "../../components/modal";
+interface Intolerancia{
+  nombre: string;
+  descripcion:string;
+  detalles:string;
+  imagen: string;
+}
 export default function Intolerancias() {
-  const handleSaberMas = () => {
-    alert("Más información sobre esta intolerancia.");
+  const [modalOpen, setModalOpen] = useState(false)
+  const [intoleSeleccionada, setIntoleSeleccionada] = useState<Intolerancia | null>(null);
+const intoleranciasDatos: Intolerancia[] = [
+  {
+    nombre: "Gluten",
+    descripcion: "No puedes consumir alimentos con gluten.",
+    detalles: "El gluten es una proteína presente en el trigo, cebada y centeno. Su consumo puede causar problemas digestivos y de salud en personas con intolerancia o enfermedad celíaca.",
+    imagen: gluten,
+  },
+  
+]
+  const handleSaberMas = (intolerancia: Intolerancia) => {
+  setIntoleSeleccionada(intolerancia);
+  setModalOpen(true); 
+  };
+  const handleClose = () => {
+    setModalOpen(false);
   };
 
   const handleSoy = () => {
@@ -18,64 +41,27 @@ export default function Intolerancias() {
       <div className="container">
         <h1>Intolerancias</h1>
         <div className="card-container">
+
+          {intoleranciasDatos.map((intolerancia, index) =>(
+
           <IntoleranciaCard
-            nombre="Gluten"
-            descripcion="No puedes consumir alimentos con gluten."
-            imagen={gluten}
-            buttonSaberMas={handleSaberMas}
-            buttonSoy={handleSoy}
+          key={index}
+          nombre={intolerancia.nombre}
+          descripcion={intolerancia.descripcion}
+          imagen={intolerancia.imagen}
+          buttonSaberMas={() => handleSaberMas(intolerancia)}
+          buttonSoy={handleSoy}
           />
-          <IntoleranciaCard
-            nombre="Gluten"
-            descripcion="No puedes consumir alimentos con gluten."
-            imagen={gluten}
-            buttonSaberMas={handleSaberMas}
-            buttonSoy={handleSoy}
-          />
-          <IntoleranciaCard
-            nombre="Gluten"
-            descripcion="No puedes consumir alimentos con gluten."
-            imagen={gluten}
-            buttonSaberMas={handleSaberMas}
-            buttonSoy={handleSoy}
-          />
-          <IntoleranciaCard
-            nombre="Gluten"
-            descripcion="No puedes consumir alimentos con gluten."
-            imagen={gluten}
-            buttonSaberMas={handleSaberMas}
-            buttonSoy={handleSoy}
-          />
-          <IntoleranciaCard
-            nombre="Gluten"
-            descripcion="No puedes consumir alimentos con gluten."
-            imagen={gluten}
-            buttonSaberMas={handleSaberMas}
-            buttonSoy={handleSoy}
-          />
-          <IntoleranciaCard
-            nombre="Gluten"
-            descripcion="No puedes consumir alimentos con gluten."
-            imagen={gluten}
-            buttonSaberMas={handleSaberMas}
-            buttonSoy={handleSoy}
-          />{" "}
-          <IntoleranciaCard
-            nombre="Gluten"
-            descripcion="No puedes consumir alimentos con gluten."
-            imagen={gluten}
-            buttonSaberMas={handleSaberMas}
-            buttonSoy={handleSoy}
-          />
-          <IntoleranciaCard
-            nombre="Gluten"
-            descripcion="No puedes consumir alimentos con gluten."
-            imagen={gluten}
-            buttonSaberMas={handleSaberMas}
-            buttonSoy={handleSoy}
-          />
+          
+        ))}
         </div>
       </div>
+      <Modal open={modalOpen}
+      onClose={handleClose}
+      title={intoleSeleccionada?.nombre || ""}
+      content={intoleSeleccionada?.detalles || ""}
+      />
+      
     </>
   );
 }
