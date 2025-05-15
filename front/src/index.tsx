26290bb7f0b9360b5fc70a56b0a77b43f9472afd
Mariantonieta <Mariantonietachacondibacco@gmail.com>
2025-05-15 10:12:56 +0200

    cambiando lo de las traducciones a public

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./translations/index"
+import "../public/translations/index"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {
   Home,
