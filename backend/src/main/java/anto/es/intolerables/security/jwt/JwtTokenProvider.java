c6e1e8858a76d4ef9330414016b2fac784e7a83b
Mariantonieta <Mariantonietachacondibacco@gmail.com>
2025-05-15 21:32:35 +0200

    Bug solucionado pt1

--- backend/src/main/java/anto/es/intolerables/security/jwt/JwtTokenProvider.java
+++ backend/src/main/java/anto/es/intolerables/security/jwt/JwtTokenProvider.java
@@ -29,8 +29,7 @@ public class JwtTokenProvider {
     @Value("${jwt.secret}")
     private String jwtSecret;
 
-    @Value("${jwt.expiration}")
-    private int jwtExpiration;
+    private int jwtExpiration = 86400000;
     private SecretKey getSigningKey() {
         byte[] keyBytes = Base64.getDecoder().decode(jwtSecret);
         return Keys.hmacShaKeyFor(keyBytes);
