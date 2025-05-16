397412ab757bbfa0828736c33027e80cb1ac56b5
Mariantonieta <Mariantonietachacondibacco@gmail.com>
2025-05-16 23:07:13 +0200

    añadiendo url del backend desplegado

--- front/src/services/axiosConfig.ts
+++ front/src/services/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 //Utilizo axios para las peticiones al backend
 //la instancia api para llamarla desde mis componentes
 const api = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "https://intolerables-backend.onrender.com",
 });
 //añade el token a cada peticion y lee el token del almacenamiento local
 api.interceptors.request.use((config) => {
